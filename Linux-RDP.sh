fe9e0906b2ba5892eb9fd47f6525239f6fa3c018
infinitygamer404 <59830101+infinitygamer404@users.noreply.github.com>
2021-01-12 11:50:34 +0600

    Update and rename CuteWolfieLinux.sh to Linux-RDP.sh

--- Linux-RDP.sh
+++ Linux-RDP.sh
@@ -19,13 +19,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser CuteWolfie chrome-remote-desktop
+sudo adduser mr-x chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - CuteWolfie -c """$CRP"""
+su - mr-x -c """$CRP"""
 printf 'Access Your RDP At https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
