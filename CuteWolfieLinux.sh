12c84813527a252c1e31bc3d118f73c821cf0724
infinitygamer404 <59830101+infinitygamer404@users.noreply.github.com>
2021-01-12 11:40:04 +0600

    Update CuteWolfieLinux.sh

--- CuteWolfieLinux.sh
+++ CuteWolfieLinux.sh
@@ -10,7 +10,7 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes kubuntu-full desktop-base
+apt install --assume-yes kde-standard desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/plasma-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
